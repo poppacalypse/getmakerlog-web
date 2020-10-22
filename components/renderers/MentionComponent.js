ac8360223e1851af616fa17a2e37a67dbbd9c391
Sergio Mattei <me@sergiomattei.com>
2020-10-21 21:43:32 -0400

    remove spaces from mentions

--- components/renderers/MentionComponent.js
+++ components/renderers/MentionComponent.js
@@ -5,7 +5,7 @@ export default function MentionComponent(key, result) {
 	return (
 		<Link
 			route="profile"
-			params={{ username: result[0].replace("@", "") }}
+			params={{ username: result[0].replace("@", "").replace(" ", "") }}
 			key={key}
 		>
 			<a>{result[0]}</a>
