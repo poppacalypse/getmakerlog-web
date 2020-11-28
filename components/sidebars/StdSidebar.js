9ca69b820a01d326fc5d9c68f55d8110e8dfa200
Sergio Mattei <sergiomattei@outlook.com>
2020-11-28 01:10:52 -0400

    Add a launch countdown

--- components/sidebars/StdSidebar.js
+++ components/sidebars/StdSidebar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AdSidebarCard from "./AdSidebarCard";
 import FeedbackCard from "./FeedbackCard";
+import LaunchRemainingCard from "./LaunchRemainingCard";
 
 function StdSidebar() {
 	return (
 		<>
 			<AdSidebarCard />
+			<LaunchRemainingCard />
 			<FeedbackCard />
 		</>
 	);
