b71ffe39c5fe2090560db12f39954378c2a28d95
Sergio Mattei <sergiomattei@outlook.com>
2021-02-27 17:47:13 -0400

    Fix mobile milestones link overflow

--- components/users/ProfileMenu.js
+++ components/users/ProfileMenu.js
@@ -3,13 +3,13 @@ import React from "react";
 
 export default function ProfileMenu({ user }) {
 	return (
-		<div className="flex flex-row items-center justify-center flex-grow h-full sm:flex-none">
+		<div className="flex flex-row flex-grow h-full max-w-full overflow-x-auto sm:flex-none">
 			<ActiveLink
 				route="profile"
 				params={{ username: user.username }}
 				activeClassName="text-green-500 border-b-2 border-green-500"
 			>
-				<a className="flex items-center justify-center flex-1 h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
+				<a className="flex items-center justify-center flex-grow h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
 					Feed
 				</a>
 			</ActiveLink>
@@ -18,7 +18,7 @@ export default function ProfileMenu({ user }) {
 				params={{ username: user.username }}
 				activeClassName="text-green-500 border-b-2 border-green-500"
 			>
-				<a className="flex items-center justify-center flex-1 h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
+				<a className="flex items-center justify-center flex-grow h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
 					Milestones
 				</a>
 			</ActiveLink>
@@ -27,7 +27,7 @@ export default function ProfileMenu({ user }) {
 				params={{ username: user.username }}
 				activeClassName="text-green-500 border-b-2 border-green-500"
 			>
-				<a className="flex items-center justify-center flex-1 h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
+				<a className="flex items-center justify-center flex-grow h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
 					Products
 				</a>
 			</ActiveLink>
@@ -36,7 +36,7 @@ export default function ProfileMenu({ user }) {
 				params={{ username: user.username }}
 				activeClassName="text-green-500 border-b-2 border-green-500"
 			>
-				<a className="flex items-center justify-center flex-1 h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
+				<a className="flex items-center justify-center flex-grow h-full px-6 py-4 pt-2 font-semibold text-center text-gray-500">
 					Discussions
 				</a>
 			</ActiveLink>
