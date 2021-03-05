de79537ab47d95f4a33550128999c25ea309268e
Sergio Mattei <sergiomattei@outlook.com>
2021-03-05 19:14:15 -0400

    Don't opt avatars

--- components/ui/Avatar.js
+++ components/ui/Avatar.js
@@ -2,11 +2,11 @@ import React from "react";
 import { imageUrl } from "vendor/imagekit";
 
 function Avatar({ size, user, className = "" }) {
-	const optSize = size >= 32 ? 128 : 48;
+	// const optSize = size >= 32 ? 128 : 48;
 	return (
 		<img
 			className={`h-${size} w-${size} rounded-full ` + className}
-			src={imageUrl(user.avatar, optSize)}
+			src={imageUrl(user.avatar)}
 			alt={user.username}
 		/>
 	);
